1aa164f7a1c0311091fbf839e9b283a9085c9a32
miguel <miguel_quiambao@dlsu.edu.ph>
2019-05-09 20:15:22 +0800

    :lipstick: Used bootstrap + Vue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,68 +2,45 @@
 
 @section('content')
     <div class="landing">
-        <form class="grid-container" action="" method="GET">
+
+        <div class="container">
             <div class="row">
-                <div class="twelve columns">
+                <div class="col">
                     <h1>Find Your Next Home</h1>
                 </div>
-                <div class="row">
-                    <div class="twelve columns">
-                        <div class="button-pill" id="purpose">
-                            <button data-value="rent">For Rent</button><button data-value="sale">For Sale</button>
-                            <input type="hidden" name="purpose" value="all">
-                        </div>
-
-                        <script>
-                            $("#purpose button").click((e) => {
-                                e.preventDefault();
-                                let prevSelected = $("#purpose button.selected");
-                                
-                                $("#purpose button").removeClass("selected");
-
-                                if (prevSelected.attr("data-value") !== $(e.target).attr("data-value")) {
-                                    $("[name=purpose]").val($(e.target).attr("data-value"));
-                                    $("#purpose button[data-value=" + $(e.target).attr("data-value") + "]").addClass("selected");
-                                } else
-                                    $("[name=purpose]").val("all");
-                            })
-                        </script>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="four columns">
-                        <select class="u-full-width">
-                            <option value="1">Townhouse</option>
-                            <option value="2">Commercial</option>
-                            <option value="3">Building</option>
-                            <option value="4">Condominium</option>
-                            <option value="5">Warehosue</option>
-                            <option value="6">House</option>
-                            <option value="7">Lot</option>
-                            <option value="8">Beachfront</option>
-                            <option value="9">Storage</option>
-                            <option value="10">Office</option>
-                            <option value="11">Island</option>
-                            <option value="12">Memorial Lot</option>
-                            <option value="13">Service Apartment</option>
-                            <option value="14">Condotel</option>
-                            <option value="15">Retail</option>
-                        </select>
-                    </div>
-                    <div class="eight columns">
-                        <div class="row">
-                            <div class="eight columns iconed-input">
-                                <label class="fas fa-search" for="q"></label>
-                                <input type="text" class="u-full-width" name="q" id="q" placeholder="Search here...">
-                            </div>
-    
-                            <div class="three columns centered">
-                                <input type="submit" value="Search" class="button-primary full-mobile">
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
-        </form>
-    </div>
+            <div class="row">
+                <form action="" class="container">
+                    <div class="row">
+                        <div class="col-md-3 form-group">
+                            <select class="custom-select custom-select-sm">
+                                <option value="1">Townhouse</option>
+                                <option value="2">Commercial</option>
+                                <option value="3">Building</option>
+                                <option value="4">Condominium</option>
+                                <option value="5">Warehosue</option>
+                                <option value="6">House</option>
+                                <option value="7">Lot</option>
+                                <option value="8">Beachfront</option>
+                                <option value="9">Storage</option>
+                                <option value="10">Office</option>
+                                <option value="11">Island</option>
+                                <option value="12">Memorial Lot</option>
+                                <option value="13">Service Apartment</option>
+                                <option value="14">Condotel</option>
+                                <option value="15">Retail</option>
+                            </select>
+                        </div>
+                        <div class="col-md-7">
+                            <div class="form-group">
+                                <input type="text" name="q" id="q" placeholder="Search Here..." class="form-control form-control-sm">
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <input type="submit" value="Search" class="btn btn-block btn-sm btn-primary">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
 @endsection
--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -1,42 +1,44 @@
-<nav class="navigation">
-    <div class="logo">
-        <div>
-            <img src="{{ asset('img/icons/logo.svg')}}" alt="UrHome Logo"> 
-            <span> urhome </span>
-        </div>
-
-        <div onclick="toggleExpandable('nav_expandable')">
-            <i class="mobile-only fas fa-bars"></i>
-        </div>
-    </div>
-    <div class="expandable" id="nav_expandable">
-        <ul class="links">
-            <li><a href="/">Home</a></li>
-            <li class="dropdown">
-                <div>
-                    <a href="/properties">Properties <i class="wide-only inline fas fa-caret-down"></i> </a>
-                    <i class="fas fa fa-caret-down mobile-only" onclick="toggleExpandable('properties')"></i>
-                </div>
-    
-                <ul id="properties">
-                    <li><a href="">View Properties</a></li>
-                    <li><a href="">Search Properties</a></li>
-                </ul>
+<nav class="navbar navbar-expand-md navbar-dark fixed-top" id="nav">
+    <a class="navbar-brand" href="#">
+        <img src="{{ asset('img/icons/logo.svg') }}" alt=""> 
+        <span>urhome</span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
             </li>
-            <li><a href="/about">About Us</a></li>
-            <li><a href="/contact">Contact Us</a></li>
-        </ul>
-    
-        <ul class="account">
-            <li class="dropdown">
-                <div>
-                    <a><i class="far fa-user-circle"></i> Account </a>
-                    <i class="fas fa fa-caret-down mobile-only" onclick="toggleExpandable('account')"></i>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="propertiesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Properties
+                </a>
+                <div class="dropdown-menu" aria-labelledby="propertiesDropdown">
+                    <a class="dropdown-item" href="#">View Properties</a>
+                    <a class="dropdown-item" href="#">Search Properties</a>
                 </div>
-                <ul id="account">
-                    <li><a href=""><i class="fas fa-sign-in-alt"></i> Log In</a></li>
-                    <li><a href=""><i class="far fa-clipboard"></i> Register</a></li>
-                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact Us</a>
+            </li>
+            
+        </ul>
+        <ul class="navbar-nav"> 
+            <li class="nav-item dropdown">
+                
+                <a href="#" class="nav-link dropdown-toggle" id="accountDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="far right fa-user-circle"></i>
+                    Account
+                </a>
+                <div class="dropdown-menu" aria-labelledby="accountDropdown">
+                    <a href="#" class="dropdown-item">Log In</a>
+                    <a href="#" class="dropdown-item">Register</a>
+                </div>
             </li>
         </ul>
     </div>
@@ -44,15 +46,9 @@
 
 <script>
     $(window).scroll(() => {
-        if ($(window).scrollTop() > 0 && $(window).width() >= 640)
-            $(".navigation").addClass("moved");
-        else 
-            $(".navigation").removeClass("moved");
+        if ($(window).scrollTop() > 0)
+            $("#nav").addClass("moved");
+        else
+            $("#nav").removeClass("moved");
     })
 </script>
-
-<script>
-        function toggleExpandable(id) {
-            $("#" + id).toggleClass("expanded");
-        }
-    </script>
